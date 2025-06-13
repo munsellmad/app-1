2ea74dd0ed6c53bcda20e3fedb542ea5fd7fc1d0
Madelyn <madelyn@Mac.attlocal.net>
2025-06-13 14:15:50 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function handleClick(){
+    alert("DON'T CLICK ME!");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +12,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}>Click Here!</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
